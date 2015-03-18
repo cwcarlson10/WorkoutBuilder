89697141418660e5a09820b2dfd6230bee6ac187
Chris Carlson <cwcarlson10@gmail.com>
2015-03-17 22:32:56 -0600

    added some exercises to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Exercise.create!(
+  name: 'Plank',
+  description: 'Get into a position on the floor supporting your weight on your forearms and toes. Arms are bent and directly below the shoulder.  Keep your body straight at all times and hold this position as long as possible. To increase difficulty an arm or leg can be raised while performing this exercise.'
+  )
+Exercise.create!(
+  name: 'Push ups',
+  description: 'Start with your body streched, your hands are shoulder-wide appart on the ground. Push yourself off the ground till you strech your arms. The back is always straight and as well as the neck (always look to the ground). Lower yourself to the initial position and repeat.'
+  )
+Exercise.create!(
+  name: 'Squats',
+  description: 'Go now slowly down, till your thighs are parallel with the floor, not lower. The knees point outwards, your butt, out. Make a small pause of 1 second and with as much energy as you can, push the weight up. Make a pause of 2 seconds and repeat.'
+  )
+Exercise.create!(
+  name: 'Pull-ups',
+  description: 'Grab the pull up bar with a wide grip, the body is hanging freely. Keep your chest out and pull yourself up till your chin reaches the bar or it touches your neck, if you want to pull behind you. Go with a slow and controlled movement down, always keeping the chest out'
+  )
