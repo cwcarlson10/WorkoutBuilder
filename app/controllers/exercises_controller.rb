6522f3fd154adfa8247d8e6a0fa634506d720271
Chris Carlson <cwcarlson10@gmail.com>
2015-03-17 22:09:33 -0600

    created exercise mvc and added Crud to controller

---
+++ app/controllers/exercises_controller.rb
@@ -0,0 +1,50 @@
+class ExercisesController < ApplicationController
+  before_action :set_athlete, only:[:show, :edit, :update, :destroy]
+
+  def index
+    @exercises = Exercise.all
+  end
+
+  def show
+  end
+
+  def new
+    @exercise = Exercise.new
+  end
+
+  def create
+    @exercise = Exercise.new(exercise_params)
+    if @exercise.save
+      redirect_to @exercise
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @exercise.update_attributes(exercise_params)
+      redirect_to @exercise
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @exercise.destroy
+    redirect_to exercise_path
+  end
+
+  private
+
+    def exercise_params
+      params.require(:exercise).permit(:name, :description)
+    end
+
+    def set_exercise
+      @exercise = Exercise.find(params[:id])
+    end
+
+end
