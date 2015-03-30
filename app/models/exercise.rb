97534a74f18df14e14b93ea5e2fc15b04afbbc21
Chris Carlson <cwcarlson10@gmail.com>
2015-03-29 19:34:16 -0600

    deleted leg model

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,10 @@
 class Exercise < ActiveRecord::Base
   has_many :instructions
-  has_many :legs
+  has_many :workouts, through: :instructions
+
+  scope :quads, -> { where(muscle_group: "quads") }
+  scope :hamstrings, -> { where(muscle_group: "hamstrings") }
+  scope :glutes, -> { where(muscle_group: "glutes") }
+  scope :calves, -> { where(muscle_group: "calves") }
+
 end
