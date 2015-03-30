4f9106f495725593997f56b63befb0ff7d61031c
Chris Carlson <cwcarlson10@gmail.com>
2015-03-29 19:33:45 -0600

    deleted the leg model

--- test/factories/legs.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :leg do
-    quads "MyString"
-hamstring "MyString"
-glutes "MyString"
-calves "MyString"
-  end
-
-end
--- test/models/leg_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class LegTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
